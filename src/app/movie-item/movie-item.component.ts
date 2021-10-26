3ff062e26d78019c311b09b5bf0eff033699488f
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-26 11:52:34 +0300

    removed movie item component's redundant dependency on database

--- src/app/movie-item/movie-item.component.ts
+++ src/app/movie-item/movie-item.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { MovieDataService } from '../movie-data.service';
 
 @Component({
   selector: 'app-movie-item',
@@ -7,11 +6,5 @@ import { MovieDataService } from '../movie-data.service';
   styleUrls: ['./movie-item.component.css']
 })
 export class MovieItemComponent {
-  dataBase : any;
-  @Input() movieIndex!: number;
-  picUrl = '';
-  
-  constructor(private movieDataService: MovieDataService) { 
-    movieDataService.nowPlayingDBPromise.then(data => this.dataBase = data.results);
-  }
+  @Input() posterUrl = '';
 }
