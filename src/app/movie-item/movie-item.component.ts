e592716162ec104f73a64130098decf33f57c4cb
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-26 12:02:11 +0300

    moved base url in separate variable

--- src/app/movie-item/movie-item.component.ts
+++ src/app/movie-item/movie-item.component.ts
@@ -6,5 +6,13 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./movie-item.component.css']
 })
 export class MovieItemComponent {
+  @Input() posterPath = '';
   @Input() posterUrl = '';
+  posterImageBaseUrl = 'http://image.tmdb.org/t/p/w342';
+  
+
+  constructor() {
+    this.posterUrl = this.posterImageBaseUrl + this.posterPath;
+  }
+
 }
