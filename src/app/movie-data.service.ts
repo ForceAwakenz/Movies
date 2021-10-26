8952ab6401567efb680e621d69bec9b4720ddf7d
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-26 09:54:43 +0300

    returned injectable decorator to service

--- src/app/movie-data.service.ts
+++ src/app/movie-data.service.ts
@@ -1,8 +1,8 @@
-// import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
+@Injectable({
+  providedIn: 'root'
+})
 export class MovieDataService {
   baseUrl = 'http://api.themoviedb.org/';
   nowPlayingDBPromise: Promise<any>;
