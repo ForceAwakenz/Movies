3e7151a4fb697d7394296745c9beeaf7dd09e785
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-27 16:07:00 +0300

    added function type into movie data service

--- src/app/movie-data.service.ts
+++ src/app/movie-data.service.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieDataService {
-  movieApiBaseUrl = 'http://api.themoviedb.org/3/movie/';
-  movieApiKey = '?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c';
+  private movieApiBaseUrl = 'http://api.themoviedb.org/3/movie/';
+  private movieApiKey = 'ebea8cfca72fdff8d2624ad7bbf78e4c';
 
   constructor(private http: HttpClient) { 
   }
 
-  getNowPlayingMovies() {
-    return this.http.get(`${this.movieApiBaseUrl}now_playing${this.movieApiKey}`)
+  getNowPlayingMovies(): Observable<any> {
+    return this.http.get(`${this.movieApiBaseUrl}now_playing?api_key=${this.movieApiKey}`)
     .pipe( map((data: any) => data.results) );
   }
 
