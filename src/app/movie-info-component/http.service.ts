a3d7eb607980032d2743aecbb30342104810c81c
Алена Балжи <balzhialena94@gmail.com>
2021-10-28 16:12:24 +0300

    add background img and made edits

--- src/app/movie-info-component/http.service.ts
+++ src/app/movie-info-component/http.service.ts
@@ -1,12 +1,14 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IMovie} from './Movie'; 
   
 @Injectable()
-export class HttpService{
+export class MovieService{
   
     constructor(private http: HttpClient){ }
       
-    getData(){
-        return this.http.get('http://api.themoviedb.org/3/movie/674025?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c')
+    getMovieById(id: string):Observable<IMovie> { 
+        return this.http.get<IMovie>('http://api.themoviedb.org/3/movie/674025?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c')
     }
 }
--- src/app/movie-info-component/movie-info-component.component.ts
+++ src/app/movie-info-component/movie-info-component.component.ts
@@ -1,37 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import {IMovie} from './Movie';
-import {HttpService} from './http.service';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from './http.service';
+import { IMovie } from './Movie';
+import {switchMap, take, map, filter} from 'rxjs/operators';
+
+const BASE_IMAGE_URL = 'http://image.tmdb.org/t/p/w342';
 
 @Component({
   selector: 'app-movie-info',
   templateUrl: 'movie-info-component.component.html',
   styleUrls: ['movie-info-component.component.css'],
-  providers: [HttpService]
+  providers: [MovieService]
 })
 
 export class MovieInfoComponentComponent implements OnInit {
-  movie: IMovie | any;
-  logo:any;
-  bacground: any;
-  id: number | undefined;
-  url: string;
-  
-  constructor(private httpService: HttpService){
-    this.url='http://image.tmdb.org/t/p/w342';
+  movie!: IMovie;
+    
+  constructor(private movieService: MovieService, private activatedRoute: ActivatedRoute){
   }
 
-  getMovieById(){
-    return   this.httpService.getData()
-    .subscribe((movie:any) => {
-      this.movie=movie;
-        })
-      }
+  getMovieInfo(): void{
+    this.activatedRoute.paramMap
+      .pipe(
+        take(1),
+        filter(params => !!params.get('id')),
+        switchMap(params => this.movieService.getMovieById(params.get('id')as string))
+      ) 
+      .subscribe((movie : any) => {
+        this.movie = movie;
+          })
+        }
 
   getMovieImageUrl(imagePath: string): string { 
-    return this.url + imagePath;
+    return BASE_IMAGE_URL + imagePath;
   }
 
   ngOnInit(): void{
-    this.getMovieById();
-    }
+    this.getMovieInfo();
+  }
 }
