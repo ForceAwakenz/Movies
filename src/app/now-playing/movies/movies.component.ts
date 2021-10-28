5136228a16dd06379653a75c8b33508b8cc6e1d4
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-28 17:00:27 +0300

    moved movies and movie item modules inside the now playing folder

--- src/app/now-playing/movies/movies.component.ts
+++ src/app/now-playing/movies/movies.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieDataService } from '../movie-data.service';
+import { MovieDataService } from '../../movie-data.service';
 
 @Component({
   selector: 'app-movies',
--- src/app/now-playing/now-playing.module.ts
+++ src/app/now-playing/now-playing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieDataService } from '../movie-data.service';
 
-import { MoviesComponent } from '../movies/movies.component';
-import { MovieItemComponent } from '../movies/movie-item/movie-item.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieItemComponent } from './movie-item/movie-item.component';
 import { HighlightDirective } from '../shared/directives/highlight.directive';
 import { NowPlayingComponent } from '../now-playing/now-playing.component';
 
