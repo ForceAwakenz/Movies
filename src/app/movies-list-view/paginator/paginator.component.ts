5f95785054bf45be13142ef296538bb62bc94dac
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-29 17:15:02 +0300

    implemented paginator user interface logic

--- src/app/movies-list-view/paginator/paginator.component.ts
+++ src/app/movies-list-view/paginator/paginator.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { MovieDataService } from 'src/app/shared/services/movie-data.service';
+
+type INumberTrio = [number, number, number];
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.css']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit{
+  @Input() pagesTotal: number = 10;
+  currentPage: number = 1;
+  pageNumberTrio: INumberTrio = [1, 2, 3];
 
-  constructor() { }
+  constructor(private movieDataService: MovieDataService,
+    private router: Router,
+    private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.currentPage = params.currentPage || this.currentPage;
+      this.renderPaginator(this.currentPage, this.pagesTotal);
+    });
+  }
+
+  // navigateTo(page) {
+  //   this.router.navigate()
+  // }
+
+  renderPaginator(toPage: number, pagesTotal: number): void  {
+    let trioShifter = 0;
+    if (!this.existsIn(toPage - 1, pagesTotal)) {
+      trioShifter++;
+    }
+    if (!this.existsIn(toPage + 1, pagesTotal)) {
+      trioShifter--;
+    }
+    let currentPosition = toPage + trioShifter;
+    this.pageNumberTrio = [ currentPosition - 1 , currentPosition, currentPosition + 1];
+    console.log(this.pageNumberTrio);
+  }
+
+  public existsIn(targetNumber: number, inRange: number): boolean {
+    return ( targetNumber >= 1 && targetNumber <= inRange) ? true : false;
   }
 
 }
