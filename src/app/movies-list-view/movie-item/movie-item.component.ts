a2ff4567824cefc959480d7851a8f1334550d3ca
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-11-01 14:21:21 +0200

    moved posterNotFoundUrl into separate variable

--- src/app/movies-list-view/movie-item/movie-item.component.ts
+++ src/app/movies-list-view/movie-item/movie-item.component.ts
@@ -8,10 +8,11 @@ import { Component, Input } from '@angular/core';
 export class MovieItemComponent {
   @Input() posterPath = '';
   private posterImageBaseUrl = 'http://image.tmdb.org/t/p/w342';
+  private posterNotFoundUrl = 'https://www.2queue.com/2queue/wp-content/uploads/sites/6/tdomf/4299/movie-poster-coming-soon.png';
 
   get posterUrl(): string {
     if (this.posterPath == null) {
-      return 'https://www.2queue.com/2queue/wp-content/uploads/sites/6/tdomf/4299/movie-poster-coming-soon.png';
+      return this.posterNotFoundUrl;
     }
     return `${this.posterImageBaseUrl}${this.posterPath}`;
   }
