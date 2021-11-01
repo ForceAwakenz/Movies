0aee49f31d28f7fefd0d67194415797daa5c21b9
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-11-01 11:21:43 +0200

    fixed missed poster issue

--- src/app/movies-list-view/movie-item/movie-item.component.ts
+++ src/app/movies-list-view/movie-item/movie-item.component.ts
@@ -10,6 +10,9 @@ export class MovieItemComponent {
   private posterImageBaseUrl = 'http://image.tmdb.org/t/p/w342';
 
   get posterUrl(): string {
+    if (this.posterPath == null) {
+      return 'https://www.2queue.com/2queue/wp-content/uploads/sites/6/tdomf/4299/movie-poster-coming-soon.png';
+    }
     return `${this.posterImageBaseUrl}${this.posterPath}`;
   }
 
