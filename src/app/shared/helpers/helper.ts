eadf5838f77f53b942cf83614da1c89ded048639
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-11-11 17:15:42 +0200

    cosmetical changes

--- src/app/shared/helpers/helper.ts
+++ src/app/shared/helpers/helper.ts
@@ -1,16 +1,15 @@
 import { IMovie } from "../interfaces/movie.interface";
 
-const posterNotFoundUrl = 'https://www.2queue.com/2queue/wp-content/uploads/sites/6/tdomf/4299/movie-poster-coming-soon.png';
-const posterImageBaseUrl = 'http://image.tmdb.org/t/p/w342';
+const POSTER_NOT_FOUND_URL = 'https://www.2queue.com/2queue/wp-content/uploads/sites/6/tdomf/4299/movie-poster-coming-soon.png';
+const POSTER_IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/w342';
 
 export class Helper {
 
     static emptyPosterResolver(movieList: IMovie[]): IMovie[] {
         return movieList.slice().map((movie: IMovie) => {
             movie['poster_path'] = movie['poster_path'] === null
-            ? posterNotFoundUrl
-            : posterImageBaseUrl + movie['poster_path'];
-            console.log(movie['poster_path'])
+                ? POSTER_NOT_FOUND_URL
+                : POSTER_IMAGE_BASE_URL + movie['poster_path'];
             return movie;
         });
     }
